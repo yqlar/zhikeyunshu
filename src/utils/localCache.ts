357606f6a8f493532d93f38e6e179ccd364c1527
yang <>
2023-05-13 22:58:30 +0800

    fix: 修复localstorage

--- src/utils/localCache.ts
+++ src/utils/localCache.ts
@@ -1,10 +1,11 @@
+import _isNil from 'lodash/isNil'
 export const setLocalStorage = (name: string, data: any) => {
     localStorage?.setItem(name, JSON.stringify(data))
 }
 
 export const getLocalStorage = (name) => {
   const d = localStorage?.getItem(name)
-  if (d === null) {
+  if (!d || _isNil(d)) {
     return null
   } else {
     return JSON.parse(d)
