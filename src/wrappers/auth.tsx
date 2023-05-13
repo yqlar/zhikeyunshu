27fc4b8ac734d461653998a102a6e0a11ddaaee1
yang <>
2023-05-14 01:29:24 +0800

    fix

--- src/wrappers/auth.tsx
+++ src/wrappers/auth.tsx
@@ -1,20 +1,18 @@
-import {Navigate} from 'umi'
+import {Navigate, useLocation} from 'umi'
 import {getLocalStorage} from '@/utils/localCache'
 
 export const Auth = (Component) => () => {
+  const pathname = useLocation().pathname
   const isLogin = getLocalStorage('isLogin')
-  console.log('-- isLogin: ', isLogin)
+
   if (isLogin) {
-    console.log('-- Component.name: ', Component.name)
-    if (Component.name !== 'HomePage') {
-      console.log('-- 11: ', )
+    if (pathname !== '/') {
       return <Component/>
     } else {
-      console.log('-- 2: ', )
       return <Navigate to="/chat"/>
     }
   } else {
-    if (Component.name === 'HomePage') {
+    if (pathname === '/') {
       return <Component/>
     } else {
       return <Navigate to="/"/>
